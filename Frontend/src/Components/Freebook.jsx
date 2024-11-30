ac90e72f11e9021b1715e8097187f719543a73e8
Niharika <116252728+Niharika1183@users.noreply.github.com>
2024-11-30 23:02:03 +0530

    Update Freebook.jsx

--- Frontend/src/Components/Freebook.jsx
+++ Frontend/src/Components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
     useEffect(()=>{
       const getBook = async()=>{
       try {
-      const res=await  axios.get("https://bookhaven-backend-ytgt.onrender.com/book");
+      const res=await  axios.get("https://bookhaven-bac.onrender.com/book");
     
       const data= res.data.filter((data)=>data.category==="Free");
       console.log(data);
